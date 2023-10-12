413a08a4dab516623be8a91ea9a756b39eb1c400
angelatyk <angelatyk@gmail.com>
2023-10-12 01:30:11 -0400

    Added sign out button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+	const { data: session } = useSession();
+
 	return (
 		<main>
 			<h1>Home</h1>
-			<button onClick={() => signIn()}>Sign in</button>
+			{session ? <button onClick={() => signOut()}>Sign Out</button> : <button onClick={() => signIn()}>Sign in</button>}
 		</main>
 	);
 }
