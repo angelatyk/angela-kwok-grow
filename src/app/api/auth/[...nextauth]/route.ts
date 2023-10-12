852eda33bb9fd64cdf8fea3c57f92b73340a24da
angelatyk <angelatyk@gmail.com>
2023-10-12 00:47:33 -0400

    Initial setup with Prisma to create new user on first sign in.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,48 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const prisma = new PrismaClient();
+
 const handler = NextAuth({
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
 		}),
 	],
+	callbacks: {
+		async signIn({ user, account, profile, email, credentials }) {
+			if (account) {
+				// Check if the user has successfully signed in with Google
+				if (account.provider === "google") {
+					if (!user.email) {
+						return false; // Reject user
+					}
+
+					// Check if the user already exists in database
+					const existingUser = await prisma.user.findUnique({
+						where: { email: user.email },
+					});
+
+					if (!existingUser) {
+						// Create a user if they don't exist
+						await prisma.user.create({
+							data: {
+								name: user.name,
+								email: user.email,
+								image: user.image,
+							},
+						});
+					}
+					return true; // Return true to allow the sign-in
+				}
+			}
+			return false; // Reject user
+		},
+	},
 });
 
 export { handler as GET, handler as POST };
-
