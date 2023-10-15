08616f0c00635b6a8b6b9c79eab9882bb9663848
angelatyk <angelatyk@gmail.com>
2023-10-15 04:39:45 -0400

    Added active state for navigation buttons. Added Avatar to Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,32 @@
+"use client";
+
 import Button from "@/components/Button/Button";
 import Logo from "@/components/Logo/Logo";
-import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Avatar from "../Avatar/Avatar";
 import "./Header.scss";
 
-export default async function Header() {
-	const session = await getServerSession();
+export default function Header() {
+	const { data: session } = useSession();
+	const pathname = usePathname();
 
 	return (
 		<header className="header">
 			<Logo />
 			{session && (
-				<nav className="nav-bar">
-					<Link href="/todo">
-						<Button label="Todo" buttonType="button" />
-					</Link>
-					<Link href="/catalog">
-						<Button label="Catalog" buttonType="button" />
-					</Link>
-				</nav>
+				<>
+					<nav className="nav-bar">
+						<Link href="/todo">
+							<Button label="Todo" buttonType="button" className={pathname.includes("/todo") ? "--active" : ""} />
+						</Link>
+						<Link href="/catalog">
+							<Button label="Catalog" buttonType="button" className={pathname.includes("/catalog") ? "--active" : ""} />
+						</Link>
+					</nav>
+					<Avatar avatarImage={session.user.image} avatarAlt={session.user.name + " Profile Picture"} />
+				</>
 			)}
 		</header>
 	);
