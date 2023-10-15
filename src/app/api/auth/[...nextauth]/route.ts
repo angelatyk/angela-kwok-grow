42af62ed6533ee20a72813ac007ad32fd83223ec
angelatyk <angelatyk@gmail.com>
2023-10-15 14:27:47 -0400

    Added NextAuth Secret.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,6 +14,7 @@ const handler = NextAuth({
 		}),
 	],
 	session: { strategy: "jwt" },
+	secret: process.env.NEXTAUTH_SECRET,
 	callbacks: {
 		async signIn({ user, account, profile, email, credentials }) {
 			if (account) {
