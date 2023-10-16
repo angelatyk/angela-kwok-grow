49f945bc1068d89d7d261d6b4fdeecdde19d895e
angelatyk <angelatyk@gmail.com>
2023-10-15 20:04:24 -0400

    Added Load More functionality.

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,24 +1,39 @@
+"use client";
+
+import Button from "@/components/Button/Button";
 import PlantCard from "@/components/PlantCard/PlantCard";
+import { useEffect, useState } from "react";
 import "./catalog.scss";
 
-async function getPlantList() {
-	const apiKey = process.env.NEXT_PUBLIC_PERENUAL_API_KEY;
-	const perenualApiURL = "https://perenual.com/api/species-list?key=";
-	const response = await fetch(`${perenualApiURL}${apiKey}&edible=1`);
-	if (!response.ok) {
-		throw new Error(`There was an issue retriving data from the API: ${response.status}`);
-	}
-	const result = await response.json();
-	return result?.data as any[];
-}
+export default function Catalog() {
+	const [plantList, setPlantList] = useState<any[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [count, setCount] = useState(1);
 
-export default async function Catalog() {
-	const plantList = await getPlantList();
+	useEffect(() => {
+		const getPlantList = () => {
+			setLoading(true);
+			const apiKey = process.env.NEXT_PUBLIC_PERENUAL_API_KEY;
+			const perenualApiURL = "https://perenual.com/api/species-list?key=";
+			fetch(`${perenualApiURL}${apiKey}&edible=1&page=${count}`)
+				.then((res) => res.json())
+				.then((json) => {
+					setPlantList((plantList) => [...plantList, ...json.data]);
+					setLoading(false);
+				})
+				.catch((error) => {
+					throw new Error(`There was an issue retriving data from the API: ${error}`);
+				});
+		};
+
+		getPlantList();
+	}, [count]);
 
 	return (
 		<main>
 			<h1 className="headline-padding">Catalog</h1>
-			<div className="plant-list">{plantList && plantList.map((plant: any) => <PlantCard key={plant.id} plant={plant} />)}</div>
+			<div className="plant-list">{plantList && plantList.map((plant: any, index: number) => <PlantCard key={index} plant={plant} />)}</div>
+			{count <= 100 && <Button label={loading ? "Loading..." : "Load More"} buttonType="button" className="load-more-button" actionFunction={() => setCount(count + 1)} />}
 		</main>
 	);
 }
