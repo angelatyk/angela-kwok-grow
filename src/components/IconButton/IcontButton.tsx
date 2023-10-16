7584d5a6569973308321b528022e1eca97ebd935
angelatyk <angelatyk@gmail.com>
2023-10-15 22:43:47 -0400

    Use Material UI icons instead. Also added animation for user interaction.

--- src/components/IconButton/IcontButton.tsx
+++ src/components/IconButton/IcontButton.tsx
@@ -1,3 +1,6 @@
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { MouseEventHandler } from "react";
 import "./IconButton.scss";
 
@@ -7,7 +10,15 @@ type Props = {
 };
 
 function IconButton({ actionType, actionFunction }: Props) {
-	return actionType && <div className={"icon-button " + actionType} onClick={actionFunction}></div>;
+	return (
+		actionType && (
+			<div className="icon-button" onClick={actionFunction}>
+				{actionType === "add" && <AddIcon fontSize="inherit" />}
+				{actionType === "delete" && <DeleteIcon fontSize="inherit" />}
+				{actionType === "favourite" && <FavoriteBorderIcon fontSize="inherit" />}
+			</div>
+		)
+	);
 }
 
 export default IconButton;
